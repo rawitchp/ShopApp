3f15657878092c09b5841a093f1f5460ca6aace9
Rawitch Payakkawan <rawitch13@gmail.com>
2023-04-24 14:05:04 +0700

    fetch data

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, ScrollView, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
 
 export default function App() {
+  const [products, setProducts] = useState([]);
+  const getAllProducts = async () => {
+    const data = await fetch('https://dummyjson.com/products/').then((res) =>
+      res.json()
+    );
+    // console.log(data.products);
+    setProducts(data.products);
+  };
+  // console.log(products.products[0]);
+  // console.log(products);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 m-10 items-center">
+      <ScrollView>
+        {products.map((item) => {
+          return <Text key={item.id}>{item.title}</Text>;
+        })}
+      </ScrollView>
+
+      <Pressable
+        onPress={getAllProducts}
+        className="bg-red-500 rounded w-20 h-10 items-center justify-center"
+      >
+        <Text className="text-white">Fetch it!</Text>
+      </Pressable>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
